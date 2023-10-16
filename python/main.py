0d8a16fe3a42655ae011d5d949a6d7e0dac4d5a0
raoulspronck <raoul.spronck@gmail.com>
2023-10-16 13:25:54 +0200

    Initial commit

---
+++ python/main.py
@@ -0,0 +1,7 @@
+import sys 
+import time
+
+while True:   
+    sys.stdout.write('Hello from python script from stdout')
+    print("Hello from python script from console")
+    time.sleep(1)
---
+++ tauri-sidecar/src-tauri/src/main.rs
@@ -0,0 +1,28 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use tauri::{
+  api::process::{Command, CommandEvent}
+};
+
+fn main() {
+  tauri::Builder::default()
+  .setup(|_app| {
+    tauri::async_runtime::spawn(async move {
+      let (mut rx, _child) = Command::new_sidecar("main")
+        .expect("failed to setup `app` sidecar")
+        .spawn()
+        .expect("Failed to spawn packaged node");
+
+      while let Some(event) = rx.recv().await {
+        if let CommandEvent::Stdout(line) = event {
+          println!("{}", format!("'{}'", line));
+        }
+      }
+    });
+
+    Ok(())
+  })
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
+}
